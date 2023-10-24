01577f06e629be8e82056b883ec386bc6335c358
ZainMustafaaa <zainmustafaaa@hotmail.com>
2023-10-24 16:54:18 +0500

    fix: port number picked frome env

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
     console.log('Server is listening on port', PORT);
